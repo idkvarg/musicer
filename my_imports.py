29101e989e07373c3513f1a5491a786fcbdc4312
Arash Nemat Zadeh <arashnm80@gmail.com>
2025-09-17 21:39:25 +0000

    edit imports

--- my_imports.py
+++ my_imports.py
@@ -1,3 +1,4 @@
+# my defined files
 from variables import *
 from db_functions import *
 from functions import *
@@ -5,14 +6,21 @@ from mp3 import *
 from log import *
 from spotify import *
 from mp3 import *
+from redis_fast_cache import get_telegram_audio_id_cached
 
+# general imports
 import os
 import re
-import csv
 import datetime
+import traceback
+import asyncio
 
+# telebot
 import telebot
 from telebot import types
+from telebot.async_telebot import AsyncTeleBot
+from telebot.types import Update # for new webhook system
+from telebot.types import InlineQueryResultCachedAudio, ReactionTypeEmoji
 
 import threading # to use lock
 import time # for sleep
@@ -23,3 +31,5 @@ import random # to choose random spotify app to be used in api
 
 import sqlite3 # to use sqlite3 database
 import boto3 # for S3 upload
+
+from fastapi import FastAPI, Request, Response
